4b344975783b051bf452e111695ba21f459c8465
Theocode12 <96832283+Theocode12@users.noreply.github.com>
2023-01-24 09:50:09 +0100

    Update 3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,8 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index=None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         The goal here is that if between two queries,
         certain rows are removed from the dataset, the user
