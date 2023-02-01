6c2b8747e88d72abcced13378928910266eed9f2
Theocode12 <vchidubem9@gmail.com>
2023-02-01 19:21:27 +0100

    appfile

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
-"""Flask app"""
-
-from flask import Flask, request, render_template
-from flask_babel import Babel, _
+""" module's doc str """
 
 
-class Config(object):
-    """config class"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+from typing import List, Dict, Union, Sequence, Callable, Any
+from flask_babel import Babel
+from flask import Flask, render_template, g, request
 
 
 app = Flask(__name__)
+babel = Babel(app, default_locale='en', default_timezone='UTC')
+
+
+class Config(object):
+    """ config for babel """
+    LANGUAGES = ['en', "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
-babel = Babel(app, default_locale="en", default_timezone="UTC")
 
 
 @babel.localeselector
-def get_locale():
-    """Returns current locale"""
-    lang = request.args.get("locale")
-    if lang and lang in Config.LANGUAGES:
-        return lang
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale() -> Union[str, None]:
+    """ override default fet_locale """
+    locale = request.args.get('locale', None)
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
-
-
-@app.route("/")
-def index():
-    """Index page"""
-    return render_template("4-index.html")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """ root path """
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+    
